8c5ae1910ea28c0baed57b38e5f61371c3180031
Ali Kürşad Çorapsız <KursadCor@outlook.com>
2021-03-06 16:25:23 +0300

    6 mart

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,6 +10,9 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
         public static string MaintenanceTime = "Bakım var.";
         public static string ProductsListed = "Ürünler Listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 15 ürün olabilir.";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün mevcut.";
+        public static string CategoryLimitExceeded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
         //public static string UnitPriceInvalid = "Fiyat 0'dan küçük olamaz";
     }
 }
