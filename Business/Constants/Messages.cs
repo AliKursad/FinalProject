4abb64638d3da33f84a2753d88a0f353a0956475
Ali Kürşad Çorapsız <KursadCor@outlook.com>
2021-03-09 20:51:29 +0300

    9 mart

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,6 +15,13 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 15 ürün olabilir.";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün mevcut.";
         public static string CategoryLimitExceeded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
+        public static string AuthorizationDenied = "Yetkiniz yok!";
+        public static string UserRegistered = "Kayıt olundu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı!";
+        public static string PasswordError = "Parola hatalı!";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string UserAlreadyExists = "Kullanıcı mevcut!";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
         //public static string UnitPriceInvalid = "Fiyat 0'dan küçük olamaz";
     }
 }
